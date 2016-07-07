a5275e59d275cbbb95ed2fdf66ad7b700cc93b1c
Otto Kartye <ottokartye@gmail.com>
2016-07-07 08:58:38 +0200

    Fixed props on some elements

--- components/Articles/Article.js
+++ components/Articles/Article.js
@@ -5,9 +5,9 @@ export default class Article extends React.Component {
 	render() {
 		return (
 			<article>
-				<Header title={this.props.title} content={this.props.content} publishedDate={this.props.publishedDate} />
+				<Header title={this.props.title} publishedDate={this.props.publishedDate} />
 				<p>{this.props.content}</p>
-				<a href="{this.props.link}">Go to page</a>
+				<a href={this.props.link}>Go to page</a>
 			</article>
 		);
 	}
--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,35 @@ import React from "react";
 import ArticleBox from "./Articles/ArticleBox";
 
 export default class Layout extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			list: []
+		};
+	}
+
+	loadNewsFromServer() {
+		var subject = this.props.subject;
+		var numberOfItems = this.props.numberOfItems;
+		$.ajax({
+		  url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=' + numberOfItems + '&callback=?&q=' + encodeURIComponent('http://news.google.com/news?q=' + subject + '&output=rss'),
+		  dataType : 'json',
+		  success  : function (data) {
+	  		if (data.responseData.feed && data.responseData.feed.entries) {
+	  			console.log(data.responseData.feed.entries);
+		    	//this.state = {list: data.responseData.feed.entries};
+			} else {
+				console.log("No feed!");
+			}
+		  }.bind(this)
+		});		
+	}
+
+	componentDidMount() {
+		//setTimeout(this.loadNewsFromServer, 2000);
+	}
+
 	render() {
 		return (
 			<h1>News feed</h1>
